1061e70f5b5a9231c2a281e56782c1d169bbdde4
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-19 00:39:40 +0100

    Form component added

---
+++ src/Form/index.js
@@ -0,0 +1,42 @@
+import "./style.css";
+
+const Form = () => (
+  <form className="form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend">Co chcesz przeliczyć?</legend>
+      <p>
+        <label>
+          <span className="form__label">Kwota w PLN*:</span>
+          <input
+            className="form__field"
+            type="number"
+            name="amount"
+            step="0.01"
+            min="0.01"
+            placeholder="wpisz liczbę"
+            autofocus
+            required
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="form__label">Waluta:</span>
+          <select name="currency" className="form__field">
+            <option value="EUR" selected>
+              EUR - euro
+            </option>
+            <option value="GBP">GBP - funt</option>
+            <option value="USD">USD - dolar</option>
+          </select>
+        </label>
+      </p>
+    </fieldset>
+    <p>
+      <button className="form__button">Przelicz</button>
+    </p>
+    <p className="form__result"></p>
+  </form>
+);
+
+export default Form;
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
 const Header = ({ title }) => (
-  <header>
-    <h1>{title}</h1>
+  <header className="header">
+    <h1 className="header__title">{title}</h1>
   </header>
 );
 export default Header;
