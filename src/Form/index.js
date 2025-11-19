0c1281a6efe05fd60660125563887b7e849a6ce6
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-19 04:58:51 +0100

    input event set

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,44 +1,62 @@
 import "./style.css";
+import currencies from "./currencies";
+import { useState } from "react";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">Co chcesz przeliczyć?</legend>
+const Form = ({ calculateResult, result }) => {
+  const [currency, setCurrency] = useState(currencies[0].shortName);
+  const [amount, setAmount] = useState("");
 
-      <div className="form__container">
-        <p className="form__section">
-          <label>
-            <span className="form__label">Kwota w PLN*:</span>
-            <input
-              className="form__field"
-              type="number"
-              name="amount"
-              step="0.01"
-              min="0.01"
-              placeholder="wpisz liczbę"
-              autoFocus
-              required
-            />
-          </label>
-        </p>
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
 
-        <p className="form__section">
-          <label>
-            <span className="form__label">Waluta:</span>
-            <select name="currency" className="form__field" defaultValue="EUR">
-              <option value="EUR">EUR - euro</option>
-              <option value="GBP">GBP - funt</option>
-              <option value="USD">USD - dolar</option>
-            </select>
-          </label>
-        </p>
-      </div>
-    </fieldset>
-    <p>
-      <button className="form__button">Przelicz</button>
-    </p>
-    <p className="form__result"></p>
-  </form>
-);
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Co chcesz przeliczyć?</legend>
+
+        <div className="form__container">
+          <p className="form__section">
+            <label>
+              <span className="form__label">Kwota w PLN*:</span>
+              <input
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                className="form__field"
+                type="number"
+                name="amount"
+                step="0.01"
+                min="0.01"
+                placeholder="wpisz liczbę"
+                autoFocus
+                required
+              />
+            </label>
+          </p>
+
+          <p className="form__section">
+            <label>
+              <span className="form__label">Waluta:</span>
+              <select
+                name="currency"
+                className="form__field"
+                defaultValue="EUR"
+              >
+                <option value="EUR">EUR - euro</option>
+                <option value="GBP">GBP - funt</option>
+                <option value="USD">USD - dolar</option>
+              </select>
+            </label>
+          </p>
+        </div>
+      </fieldset>
+      <p>
+        <button className="form__button">Przelicz</button>
+      </p>
+      <p className="form__result"></p>
+    </form>
+  );
+};
 
 export default Form;
