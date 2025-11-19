00e30cfc925b433f47e1371c08e36515e7417fdf
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-19 04:34:28 +0100

    const currencies created

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import Container from "./Container";
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
+import { useState } from "react";
 
 function App() {
+
+const [result, setResult] = useState();
+
+const calculateResult = (currency, amount) => {
+  
+}
+
   return (
     <Container className="container">
       <Header title="Kalkulator walut" />
---
+++ src/Form/currencies.js
@@ -0,0 +1,15 @@
+const currencies = [
+  {
+    shortName: "EUR",
+    fullName: "EUR - euro",
+    rate: 4.26,
+  },
+  { shortName: "GBP",
+    fullName: "GBP - funt",
+    rate: 4.84 },
+  { shortName: "USD",
+    fullName: "USD - dolar",
+    rate: 3.7 },
+];
+
+export default currencies;
