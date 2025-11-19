a157fd8e899ff5b9cbb6ffe400111154ca65aa93
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-19 01:16:27 +0100

    Form css styles changed

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,33 +4,35 @@ const Form = () => (
   <form className="form">
     <fieldset className="form__fieldset">
       <legend className="form__legend">Co chcesz przeliczyć?</legend>
-      <p>
-        <label>
-          <span className="form__label">Kwota w PLN*:</span>
-          <input
-            className="form__field"
-            type="number"
-            name="amount"
-            step="0.01"
-            min="0.01"
-            placeholder="wpisz liczbę"
-            autofocus
-            required
-          />
-        </label>
-      </p>
-      <p>
-        <label>
-          <span className="form__label">Waluta:</span>
-          <select name="currency" className="form__field">
-            <option value="EUR" selected>
-              EUR - euro
-            </option>
-            <option value="GBP">GBP - funt</option>
-            <option value="USD">USD - dolar</option>
-          </select>
-        </label>
-      </p>
+
+      <div className="form__container">
+        <p className="form__section">
+          <label>
+            <span className="form__label">Kwota w PLN*:</span>
+            <input
+              className="form__field"
+              type="number"
+              name="amount"
+              step="0.01"
+              min="0.01"
+              placeholder="wpisz liczbę"
+              autoFocus
+              required
+            />
+          </label>
+        </p>
+
+        <p className="form__section">
+          <label>
+            <span className="form__label">Waluta:</span>
+            <select name="currency" className="form__field" defaultValue="EUR">
+              <option value="EUR">EUR - euro</option>
+              <option value="GBP">GBP - funt</option>
+              <option value="USD">USD - dolar</option>
+            </select>
+          </label>
+        </p>
+      </div>
     </fieldset>
     <p>
       <button className="form__button">Przelicz</button>
