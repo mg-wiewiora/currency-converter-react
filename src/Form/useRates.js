bd4ee91a48d448793ca87cbc37ad36af3ef6dd17
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-25 03:59:04 +0100

    useRates hook created

---
+++ src/Form/useRates.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export const useRates = () => {
+const [ratesData, setRatesData] = useState({
+    state: "loading",
+});
+
+useEffect(() => {
+    const fetchRates = async () => {
+        try {
+            const response = await fetch("url")
+
+            if(!response.ok) {
+                throw new Error(response.statusText);
+            }
+
+            const data = await response.json();
+
+            setRatesData({
+                state: "success",
+          rates: data.data,
+          date: data.meta.last_updated_at,
+            });
+        } catch {
+            setRatesData({
+                state: "error",
+            });
+        }
+    };
+    setTimeout(fetchRates,1000);
+}, []);
+
+return ratesData;
+};
