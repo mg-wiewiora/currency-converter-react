451d50cd3767d3d8ca94c957a1e976c16706b65d
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-25 03:19:39 +0100

    GlabalStyle formatting improved

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,25 +2,23 @@ import { createGlobalStyle } from "styled-components";
 import background from "./background.png";
 
 export const GlobalStyle = createGlobalStyle`
-
-html {
+  html {
     box-sizing: border-box;
-}
+  }
 
-*, ::after, ::before {
+  *, ::after, ::before {
     box-sizing: inherit;
-}
+  }
 
-body {
-  font-family: "Lato", sans-serif;
-  padding: 20px;
-  background-image: url("${background}");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-attachment: fixed;
-  min-height: 100vh;
-  background-color: ${({ theme }) => theme.color.lilac};
-}
-
+  body {
+    font-family: "Lato", sans-serif;
+    padding: 20px;
+    background-image: url("${background}");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    min-height: 100vh;
+    background-color: ${({ theme }) => theme.color.lilac};
+  }
 `;
