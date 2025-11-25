720244434cfa0778fceab00860bc13a0a5d4d0f0
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-25 06:04:40 +0100

    Footer text updated

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <Wrapper>
       <p>
-        Kursy walut pochodzą ze strony <em>currencyapi.com</em>
+        Kursy walut pochodzą ze strony <em>currencyapi.com</em> .
         <br />
         Aktualne na dzień {new Date(ratesData.date).toLocaleString("pl-PL")}.
         <br />* pole obowiązkowe
