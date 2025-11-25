5e9056aecc65cb048da2c020e62c7f9dae515067
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-25 05:25:45 +0100

    Footer updated

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,13 +1,18 @@
 import { Wrapper } from "./styled";
 
-const Footer = () => (
-  <Wrapper>
-    <p>
-      Kursy walut pochodzą ze strony <em>nbp.pl</em> z Tabeli nr 214/A/NBP/2025
-      z dnia 2025-11-04.
-      <br />* pole obowiązkowe
-    </p>
-  </Wrapper>
-);
+const Footer = ({ ratesData }) => {
+  if (ratesData?.state !== "success") return null;
+
+  return (
+    <Wrapper>
+      <p>
+        Kursy walut pochodzą ze strony <em>currencyapi.com</em>
+        <br />
+        Aktualne na dzień {new Date(ratesData.date).toLocaleString("pl-PL")}.
+        <br />* pole obowiązkowe
+      </p>
+    </Wrapper>
+  );
+};
 
 export default Footer;
